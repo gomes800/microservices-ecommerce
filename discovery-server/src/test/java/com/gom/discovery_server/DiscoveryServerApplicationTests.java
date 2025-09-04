1fae0426cc3c02f60456de189cde52593df533c8
gom-trab <jgomesleite8@gmail.com>
2025-09-04 14:58:50 -0300

    Initial commit microservices-ecommerce

---
+++ discovery-server/src/test/java/com/gom/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
