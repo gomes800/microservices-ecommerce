22aab181ffc9d96856c48af46157c98045eb0f3c
gom-trab <jgomesleite8@gmail.com>
2025-09-10 16:03:51 -0300

    feat: create order model, repositorie, service

---
+++ order-service/src/main/java/com/gom/order_service/model/Order.java
@@ -0,0 +1,58 @@
+package com.gom.order_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    private Long quantity;
+    private String status;
+
+    public Order() {}
+
+    public Order(Long id, Long productId, Long quantity, String status) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
